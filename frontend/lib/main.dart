557e533badab4fb2b11d385f80e8671dc7263980
Yerin <152307466+rlnrlnworld@users.noreply.github.com>
2024-11-19 18:56:05 +0900

    feat: 대시보드 페이지 구현 (#27)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_template.dart';
 import 'package:okay_life_app/data/auth_state.dart';
 import 'package:okay_life_app/pages/createGalaxy_page.dart';
+import 'package:okay_life_app/pages/dashboard_page.dart';
 import 'package:okay_life_app/pages/login_page.dart';
 import 'package:okay_life_app/pages/onboarding_page.dart';
 import 'package:okay_life_app/pages/splash_page.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // 디버그 배너 제거
       title: 'Okay Life App',
-      home: CreateGalaxyPage(result: "게으른 인식형 외재적 동기",),
+      home: DashboardPage(),
     );
   }
 }
