3896198c8ec9c9b50504d6ff67c914b2016b3d4a
Tvivid <tjsaud3250@gmail.com>
2024-11-20 12:42:47 +0900

    feat: ChatGPT API 설정 (#28)

---
+++ backend/src/main/java/com/spaceme/chatGPT/config/WebClientConfig.java
@@ -0,0 +1,20 @@
+package com.spaceme.chatGPT.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://api.openai.com/v1")
+                .defaultHeader("Authorization", "Bearer " + apiKey) // API 키 설정
+                .build();
+    }
+}
