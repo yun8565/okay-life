ec881cac72cb2604f1f7c6ee7e3ba4285ea479b8
eunjeongJung <gtsyej11@dgu.ac.kr>
2024-11-26 14:04:59 +0900

    fix:프롬프트 수정

--- backend/src/main/java/com/spaceme/chatGPT/service/ChatGPTService.java
+++ backend/src/main/java/com/spaceme/chatGPT/service/ChatGPTService.java
@@ -55,8 +55,8 @@ public class ChatGPTService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new NotFoundException("사용자를 찾을 수 없습니다."));
 
-        String prompt = goalRequest.goal() + "(이)라는 목표 달성을 위해 질문자의 배경에 대해 알고 싶은 3개의 질문을 생성해줘.\n " +
-                "하나의 질문은 무조건 질문자가 목표 달성을 위해 1주에 얼마나 자주 시간을 할애할 수 있는지. json 형식으로 key는 three, value는 질문 내용만";
+        String prompt = goalRequest.goal() + "(이)라는 목표를 달성하기 위한 세부 목표를 세우는데 참고할 정보가 필요해. 질문자에게 맞는 세부 목표를 세울 때, 참고할 수 있는 정보를 얻기 위한 3개의 질문을 생성해줘.\n " +
+                " json 형식으로 key는 three, value는 질문 내용만";
 
         return webClient.post()
                 .uri("/chat/completions")
@@ -75,10 +75,59 @@ public class ChatGPTService {
     }
 
     @Transactional
+    public Long generateDays(Long userId, PlanRequest planRequest) {
+        String combinedInput = "p1."+ planRequest.startDate() + "~"+ planRequest.endDate() +"중" + planRequest.days()+ "에 해당하는 일자만 남겨\n" +
+                "p2. 남은 일자들을 "+ planRequest.step() +"개의 그룹으로 나눠. 이때 각 그룹별로 포함되어있는 일자 개수가 동일해야해. 등분되지 않는 날짜는 버림처리해줘.\n" +
+                "p3.  json 형태로 반환해. 형태는 다음과 같아.\n" +
+                "{\n" +
+                "\"title\" : group,\n" +
+                "\"dates\" : [{\"date\" : \"날짜\"}]\n" +
+                "}\n" +
+                "json 값만 반환해줘."
+                ;
+
+
+        PlanResponse planResponse = webClient.post()
+                .uri("/chat/completions")
+                .bodyValue(Map.of(
+                        "model", "gpt-4o-mini",
+                        "messages", List.of(
+                                Map.of("role", "system", "content", "너는 목표를 달성하고 싶은 사용자를 위해 단계별 세부 목표와 계획을 수립해주는 전문가야.\n" +
+                                        "사용자의 input을 고려해서 단계별 세부 목표와 계획을 수립해줘.\n"),
+                                Map.of("role", "user", "content", combinedInput)
+                        ),
+                        "max_tokens", 1500
+                ))
+                .retrieve()
+                .bodyToMono(ChatGPTResponse.class)
+                .block()
+                .toResponse(PlanResponse.class);
+
+        return galaxyService.saveGalaxy(userId, planResponse, planRequest);
+    }
+
+
+
+    @Transactional
     public Long generatePlan(Long userId, PlanRequest planRequest) {
-        String combinedInput = "목표는 " + planRequest.title() + "이야 \n 아래 질의응답을 참고해서 계획을 짜줘 "
+        String combinedInput =  "input은 다음과 같아.\n" +
+                "1. 목표(String) :"+ planRequest.title() +
+                "\n 2. 질의응답 (질문(String) : 답변 (String))"
                 + planRequest.answers().stream()
-                .map(answer -> answer.question() + ": " + answer.answer());
+                .map(answer -> answer.question() + ": " + answer.answer())+
+                "\n" +
+                "너가 해야할 일은 다음과 같아.\n" +
+                "let’s think step by step."+
+                "1. 사용자의 목표를 달성하는데 필요한 세부 목표를 총" + planRequest.step() +
+                "개의 단계로 나눠서 세워. 넌 사용자를 고려한 세부 목표 수립 전문가니까 사용자의 목표와 질의응답을 잘 고려해서 세부 목표를 만들어줄 수 있어.\n" +
+                "2. 각 세부 목표를 이루기 위한 " + planRequest.days() +
+                "개의 계획을 생성해줘. 넌 사용자를 고려한 계획 수립 전문가니까 세부 목표와 질의응답 내용을 잘 고려해서 계획을 만들어줄 수 있어. 각 계획을 하루 단위로 실천해야한다는 점을 고려해서 현실적인 계획을 수립해줘. 문장을 명사로 종결해줘.\n" +
+                "3. 세부 목표와 계획을 json 형태로 반환해. 형태는 다음과 같아.\n" +
+                "{\"planets\" : [\n" +
+                "\"title\" : 세부 목표,\n" +
+                "\"missions\" : [{\"content\" : \"계획\"}]\n" +
+                "]}\n" +
+                "json 값만 반환해줘.";
 
 
          PlanResponse planResponse = webClient.post()
@@ -86,7 +135,8 @@ public class ChatGPTService {
                  .bodyValue(Map.of(
                         "model", "gpt-4o-mini",
                         "messages", List.of(
-                                Map.of("role", "system", "content", "너는 사용자의 목표와 질문에 대한 답변에 맞게 계획을 짜주는 전문가야."),
+                                Map.of("role", "system", "content", "너는 목표를 달성하고 싶은 사용자를 위해 단계별 세부 목표와 계획을 수립해주는 전문가야.\n" +
+                                        "사용자의 input을 고려해서 단계별 세부 목표와 계획을 수립해줘.\n"),
                                 Map.of("role", "user", "content", combinedInput)
                         ),
                         "max_tokens", 1500
