3a9d1478104b5aafb0830bc6144ebd8bb0d4767e
rlnrln <diiidhe@gmail.com>
2024-11-28 12:22:15 +0900

    feat: api 연결 코드 추가

--- frontend/lib/pages/setting_page.dart
+++ frontend/lib/pages/setting_page.dart
@@ -4,7 +4,13 @@ import 'package:flutter/widgets.dart';
 class SettingPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(child: Image.asset("assets/dashboard_bg.png", fit: BoxFit.cover,)),
+        ],
+      ),
+    );
   }
 
 }
