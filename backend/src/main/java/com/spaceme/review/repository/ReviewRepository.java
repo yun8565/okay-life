dcf08c1da6c91c19d12d214c0a37d8659740d9d4
진선명 <105791165+Tvivid@users.noreply.github.com>
2024-11-30 10:59:33 +0900

    feat: 회고 조회 기능 추가 (#80)

--- backend/src/main/java/com/spaceme/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/spaceme/review/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.spaceme.review.repository;
 
 import com.spaceme.review.domain.Review;
+import com.spaceme.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByUser(User user);
+    Optional<Review> findByUserAndPlanetId(User user, Long planetId);
 }
